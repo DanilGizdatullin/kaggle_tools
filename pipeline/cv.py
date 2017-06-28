f5c5eb8f2389b0eb613957de07d2ad15827b449b
danil.gizdatullin <gizdatullindanil@gmail.com>
2017-06-28 11:49:20 +0300

    add docstring

--- pipeline/cv.py
+++ pipeline/cv.py
@@ -5,11 +5,31 @@ import xgboost as xgb
 
 
 class CVSklearn(object):
+    """
+    This class helps to make cross-validation for classification or regression task by using model from scikit-learn
+    """
     def __init__(self, n_folds=5):
+        """
+        Initialize object to create cross-validation
+        :param n_folds: number of folds in cross-validation
+        :return:
+        """
         self.n_folds = n_folds
 
-    def run_cv(self, df, pipeline_train, pipeline_test, pipeline_both, model, metrics):
-
+    def run_cv(self, df, pipeline_train, pipeline_test, pipeline_both, model, metrics, is_predict_proba=False):
+        """
+        Run cross-validation. The last column after pipeline_train, pipeline_test and pipeline_both is target column.
+        :param df: pd.DataFrame with data for cv.
+        :param pipeline_train: pipeline with transformations for train data
+        :param pipeline_test: pipeline with transformations for test data
+        :param pipeline_both: pipeline with transformations for both (train, test).
+        It starts after pipeline_train and pipeline_test.
+        :param model: model from sklearn with methods fit() and predict() or predict_proba()
+        :param metrics: list of some metrics from sklearn.metrics
+        :param is_predict_proba: if it's True model uses predict_proba() method otherwise predict() method
+        :return: np.array with shape (self.n_folds, len(metrics)) where x is fold_id
+        and y is correspondent metric from metrics
+        """
         kf = KFold(n_splits=self.n_folds, shuffle=True)
 
         columns = df.columns
@@ -40,7 +60,10 @@ class CVSklearn(object):
             y_train, y_test = train_df.values[:, -1], test_df.values[:, -1]
 
             model.fit(x_train, y_train)
-            y_pred = model.predict(x_test)
+            if is_predict_proba:
+                y_pred = model.predict_proba(x_test)[:, 1]
+            else:
+                y_pred = model.predict(x_test)
 
             for i in range(len(metrics)):
                 metrics_values_for_fold[0, i] = metrics[i](y_test, y_pred)
@@ -49,10 +72,31 @@ class CVSklearn(object):
 
 
 class CVXGBoost(object):
+    """
+    This class helps to make cross-validation for classification or regression task by using model from XGBoost
+    """
     def __init__(self, n_folds=5):
+        """
+        Initialize object to create cross-validation
+        :param n_folds: number of folds in cross-validation
+        :return:
+        """
         self.n_folds = n_folds
 
     def run_cv(self, df, pipeline_train, pipeline_test, pipeline_both, metrics, dict_of_xgb):
+        """
+        Run cross-validation. The last column after pipeline_train, pipeline_test and pipeline_both is target column.
+        :param df: pd.DataFrame with data for cv
+        :param pipeline_train: pipeline with transformations for train data
+        :param pipeline_test: pipeline with transformations for test data
+        :param pipeline_both: pipeline with transformations for both (train, test).
+        It starts after pipeline_train and pipeline_test.
+        :param metrics: list of some metrics from sklearn.metrics
+        :param dict_of_xgb: dict with key 'xgb_params' and value dict with parameters for XGBoost
+        and key 'num_boost_round' and value N where N - is number of boosting rounds
+        :return: np.array with shape (self.n_folds, len(metrics)) where x is fold_id
+        and y is correspondent metric from metrics
+        """
 
         kf = KFold(n_splits=self.n_folds, shuffle=True)
 
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,11 +1,30 @@
 class Pipeline(object):
-    def __init__(self, pipeline=[]):
+    """
+    Pipeline for data transformations
+    """
+    def __init__(self, pipeline=None):
+        """
+        Initialize pipeline by using list of transformations functions.
+        :param pipeline: list of transformations functions. Transformation function is a function with
+        pd.DataFrame as the input and pd.DataFrame as the output
+        :return:
+        """
         self.pipeline = pipeline
 
     def add_transformation(self, function):
+        """
+        Add new transformation to pipeline
+        :param function: function with pd.DataFrame as the input and pd.DataFrame as the output
+        :return:
+        """
         self.pipeline.append(function)
 
     def run_pipeline(self, df):
+        """
+        Run pipeline on some pd.DataFrame df
+        :param df: pd.DataFrame
+        :return: transformed pd.DataFrame
+        """
         df_tr = df.copy()
         for func in self.pipeline:
             df_tr = func(df_tr)
@@ -13,8 +32,33 @@ class Pipeline(object):
         return df_tr
 
 
-class PipelineTrainTest(Pipeline):
+class PipelineTrainTest(object):
+    """
+    Pipeline for two datasets transformations
+    """
+    def __init__(self, pipeline=None):
+        """
+        Initialize pipeline by using list of transformations functions.
+        :param pipeline: list of transformations functions. Transformation function is a function with two
+        pd.DataFrames as the input and two pd.DataFrames as the output
+        :return:
+        """
+        self.pipeline = pipeline
+
+    def add_transformation(self, function):
+        """
+        Add new transformation to pipeline
+        :param function: function with two pd.DataFrames as the input and two pd.DataFrames as the output
+        :return:
+        """
+        self.pipeline.append(function)
+
     def run_pipeline(self, df1, df2):
+        """
+        :param df1: the first input pd.DataFrame
+        :param df2: the second input pd.DataFrame
+        :return: two transformed pd.DataFrame
+        """
         df1_tr = df1.copy()
         df2_tr = df2.copy()
         for func in self.pipeline:
